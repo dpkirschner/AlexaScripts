f5d49c8d87843171e08d6f52b011d772b6a6e548
Daniel Kirschner <dpkirschner@gmail.com>
2015-11-21 16:58:01 -0800

    generates article + noun combinations

--- src/main/java/com/dank/alexa/scripts/GenerateUtterances.java
+++ src/main/java/com/dank/alexa/scripts/GenerateUtterances.java
@@ -12,36 +12,37 @@ import com.google.common.collect.ImmutableSet;
 
 
 public class GenerateUtterances {
-	private static String postfix = ".json";
-	private static String prefix = "src/main/resources/";
+	private static String POSTFIX = ".json";
+	private static String PREFIX = "src/main/resources/";
+	private static String SPACE = " ";
+	//private static int ADJECTIVE_PERMUTATIONS_LIMIT = 2;
 	
 	private static Set<String> phrases;
 	private static Set<String> articles = ImmutableSet.of("a", "an", "the");
 	private static Set<String> utterances = new HashSet<String>();
 	
 	public static void main(String args[]) throws JsonParseException, JsonMappingException, IOException {
-		WordBank wb = new ObjectMapper().readValue(new File(prefix + "sample" + postfix), WordBank.class);
+		WordBank wb = new ObjectMapper().readValue(new File(PREFIX + "sample" + POSTFIX), WordBank.class);
 		generateNounPhrases(wb);
 		generateQuestions(wb);
 		generateImperativeCommands(wb);
-		phrases.stream().sorted().forEach(System.out::println);
+		utterances.stream().sorted().forEach(System.out::println);
 	}
 
 	
 	private static void generateNounPhrases(WordBank wb) {
 		Set<String> nouns = wb.getNouns();
-		Set<String> adjectives = wb.getNouns();
+		
+		
 		
 		//generate intermediate adjective permutation + noun
 		//store intermediate permutation
 		//generate article permutation
-
-		
 		
 		nouns.stream()
-			.forEach(s1 -> {
+			.forEach(noun -> {
 				articles.stream().forEach(
-						s2 -> phrases.add(s2 + s1)
+						article -> utterances.add(article + SPACE + noun)
 						);
 			});
 	}
